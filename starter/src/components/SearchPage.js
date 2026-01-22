c30efc0e6b49c8432244ee779f3847cad91cdc4f
Komgrich Srisak <komgrichsrisak@MacBook-Pro-khxng-Komgrich.local>
2026-01-22 17:45:29 +0700

    [FEAT]:Able to get queried books in log

--- starter/src/components/SearchPage.js
+++ starter/src/components/SearchPage.js
@@ -2,13 +2,33 @@ import "../App.css"
 import { useEffect , useState } from "react"
 import { Router , Route } from "react-router-dom"
 import MainPage from "../App.js"
+import { search , get } from "../BooksAPI.js"
 
 function SearchPage () {
 
+    const [searchText,setSearchText] = useState("")
+    const [searchResult,setSearchResult] = useState([])
+
     useEffect(() => {
+        const init_search_result = async () => {
+            const search_result = await search(searchText,20)
+            if (Array.isArray(search_result) && search_result.length) {
+                const books = await Promise.all(
+                    search_result.map(each_result => get(each_result["id"]))
+                )
+                setSearchResult(books)
+            }
+            
+        }
 
-    },[])
+        init_search_result()
+    },[searchText])
 
+    const handleSearchText = (event) => {
+        setSearchText(event.target.value)
+    } 
+
+    console.log(searchResult)
     return (
         <div>
             <div className="search-books">
@@ -30,13 +50,14 @@ function SearchPage () {
                 <div className="search-books-input-wrapper">
                 <input
                     type="text"
-                    onChange={() => {}}
+                    onChange={(event) => handleSearchText(event)}
                     placeholder="Search by title, author, or ISBN"
                 />
                 </div>
             </div>
             <div className="search-books-results">
                 <ol className="books-grid"></ol>
+                <div>{searchText}</div>
             </div>
             </div>
         </div>
